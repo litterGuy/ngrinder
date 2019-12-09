0f2b70e9b0df6bb017dda31e74b0233fd4ad77b4
王伟 <745689249@qq.com>
2019-12-09 14:33:27 +0800

    add perftest list and delete api

--- ngrinder-controller/src/main/java/org/ngrinder/feature/controller/FeatureUserController.java
+++ ngrinder-controller/src/main/java/org/ngrinder/feature/controller/FeatureUserController.java
@@ -3,12 +3,22 @@ package org.ngrinder.feature.controller;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
+import org.ngrinder.model.PerfTest;
 import org.ngrinder.model.User;
+import org.ngrinder.perftest.service.PerfTestService;
+import org.ngrinder.security.SecuredUser;
 import org.ngrinder.user.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,10 +42,12 @@ public class FeatureUserController {
 	private UserService userService;
 	@Autowired
 	private ShaPasswordEncoder passwordEncoder;
+	@Autowired
+	private PerfTestService perfTestService;
 
 	@RequestMapping(value = "login", method = RequestMethod.POST)
 	@ResponseBody
-	public Object login(String userId,String password) {
+	public Object login(String userId, String password) {
 		Map<String, Object> result = new HashMap<>();
 		result.put("code", 0);
 		if (StringUtils.isEmpty(userId) || StringUtils.isEmpty(password)) {
@@ -56,5 +68,41 @@ public class FeatureUserController {
 		}
 		result.put("data", tmp);
 		return gson.toJson(result);
+	}
+
+	@RequestMapping(value = "perfList", method = RequestMethod.GET)
+	@ResponseBody
+	public Object perfList(String userId, int page, int pageSize, String name, String queryFilter) {
+		User user = this.setUser(userId);
+		PageRequest pageRequest = new PageRequest(page, pageSize, new Sort(Sort.Direction.DESC, "id"));
+		final Page<PerfTest> testList = perfTestService.getPagedAll(user, name, null, queryFilter, pageRequest);
+		Map<String, Object> result = new HashMap<>();
+		result.put("code", 0);
+		Map<String, Object> tmp = new HashMap<>();
+		tmp.put("total", testList.getTotalElements());
+		tmp.put("list", testList.getContent());
+		result.put("data", tmp);
+		return gson.toJson(result);
+	}
+
+	@RequestMapping(value = "perfDelete", method = RequestMethod.GET)
+	@ResponseBody
+	public Object perfDelete(String userId, String ids) {
+		User user = this.setUser(userId);
+		for (String idStr : StringUtils.split(ids, ",")) {
+			perfTestService.delete(user, NumberUtils.toLong(idStr, 0));
+		}
+		Map<String, Object> result = new HashMap<>();
+		result.put("code", 0);
+		return gson.toJson(result);
+	}
+
+	private User setUser(String userId) {
+		User user = userService.getOne(userId);
+		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(new SecuredUser(user, null), null);
+		SecurityContextImpl context = new SecurityContextImpl();
+		context.setAuthentication(token);
+		SecurityContextHolder.setContext(context);
+		return user;
 	}
 }
